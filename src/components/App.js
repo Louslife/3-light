eeba2715ee273136bc814ea1f30972b28a9d4332
Louslife <louslife122@gmail.com>
2021-10-27 16:14:59 +0800

    final

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Style from "../Style..module.css";
 
 function Box(props) {
   return (
     <>
       <div className={Style.BoxBg} onClick={props.handleOpenLight}>
-        <Light open={props.open} inx="0" dataInx={props.dataInx} />
-        <Light open={props.open} inx="1" dataInx={props.dataInx} />
-        <Light open={props.open} inx="2" dataInx={props.dataInx} />
+        <Light openList={props.openList} inx="0" dataInx={props.dataInx} />
+        <Light openList={props.openList} inx="1" dataInx={props.dataInx} />
+        <Light openList={props.openList} inx="2" dataInx={props.dataInx} />
       </div>
     </>
   );
@@ -16,11 +16,7 @@ function Box(props) {
 function Light(props) {
   const defineLight = (
     <>
-      <div
-        data-inx={props.inx}
-        className={Style.BoxLight}
-        DataInx={props.dataInx}
-      ></div>
+      <div data-inx={props.inx} className={Style.BoxLight}></div>
     </>
   );
 
@@ -33,33 +29,34 @@ function Light(props) {
       ></div>
     </>
   );
-  console.log(props.dataInx,props.inx)
-  return (
-    <>
-      {props.dataInx === props.inx ? BoxLightOpen : defineLight}
-    </>
-  );
+  return <>{props.openList.includes(props.inx) ? BoxLightOpen : defineLight}</>;
 }
 
-function App(props) {
-  const [open, setOpen] = useState(false);
-  const [dataInx, setDataInx] = useState(null);
-  // 其實燈就只有三顆 
+function App() {
+  const [openList, setOpenList] = useState([]);
   const handleOpenLight = (e) => {
-    setDataInx(e.target.getAttribute("data-inx"));
-
-    if (open === false) {
-      setOpen(true);
-    } 
-    else {
-      setOpen(false);
-    }
-    // if data-inx 1~3 都亮了 就在一秒後熄燈
+    if (openList.length === 3) return
+    const inx = e.target.getAttribute("data-inx");
+    setOpenList((e) => {
+      if (e.includes(inx)) {
+        return e.filter((item) => item !== inx);
+      } else {
+        return [...e, inx];
+      }
+    });
   };
-  console.log(dataInx);
+  useEffect(() => {
+    if (openList.length === 3) {
+
+      setTimeout(() => {
+        setOpenList([]);
+      }, 1000);
+    }
+  }, [openList]);
+
   return (
     <div className="App">
-      <Box handleOpenLight={handleOpenLight} open={open} dataInx={dataInx} />
+      <Box handleOpenLight={handleOpenLight} openList={openList} />
     </div>
   );
 }
